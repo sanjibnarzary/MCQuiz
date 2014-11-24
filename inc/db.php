d9afb8387a6c58f3dcb020f23a990bb2aea141ef
Sanjib Narzary <o-._.-o@live.com>
2014-11-24 11:50:31 +0530

    database information updated...

--- inc/db.php
+++ inc/db.php
@@ -3,12 +3,15 @@
 **@author Sanjib Narzary
 */
 	class DBCon{
-			public $username;      
+		public $username;      
     		public $password;       
     		public $db;
 		function conN(){
-				$username='root';        
+			//Here you must put your database username
+			$username='root';       
+			//This field is for password, if your database have password protected user that use that password in plaintext
     		 	$password = '';        
+    		 	//This field is for database name change it for your corresponding database name
     			$db='mockquiz'; 
         		$conn = new PDO('mysql:host=127.0.0.1;dbname='.$db.'',$username, $password);
         		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
